fc305cd5706b3c2c349e9299a4be830c95881c03
Sulochana5B3 <76726071+Sulochana5B3@users.noreply.github.com>
2023-03-31 17:58:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+// data source
+
+const url = "https://raw.githubusercontent.com/Sulochana5B3/Covert-CSV-to-HTML/main/test.csv";
+
+// getData function
+
+async function getData() {
+
+    
+// Part 1: Get CSV
+
+    // fetch data
+    const response = await fetch(url);
+    // process data
+    const rawData = await response.text();
+    // publish data
+    document.getElementById("csv").innerHTML = rawData;
+
+    // developer info
+    console.log(rawData);
+    console.log("rawData type: " + typeof rawData);
+
+
+    // Part 2: CSV to JSON
+
+    // initialize variables
+    let arrayOne = rawData.split("\r\n");
+    let header = arrayOne[0].split(",");
+    let noOfRow = arrayOne.length;
+    let noOfCol = header.length;
+    let jsonData = [];
+    let i = 0;
+    let j = 0;
+
+    // for loop (rows)
+    for (i = 1; i < noOfRow - 1; i++) {
+        let obj = {};
+        let myNewLine = arrayOne[i].split(",");
+        // nested for loop (columns)
+        for (j = 0; j < noOfCol; j++) {
+            obj[header[j]] = myNewLine[j];
+        };
+        // generate JSON
+        jsonData.push(obj);
+    };
+
+    // publish data
+    document.getElementById("json").innerHTML = jsonData;
+
+    // developer info
+    console.log(jsonData);
+    console.table(jsonData);
+    console.log("jsonData type: " + typeof jsonData);
+
+    // Part 3: JSON to HTML Table
+
+    // initialize variables
+    let children = jsonData;
+    let table = document.createElement("table");
+
+    // function to generate table header row
+    function addHeaders(table, keys) {
+        let row = table.insertRow();
+        for (i = 0; i < keys.length; i++) {
+            let cell = row.insertCell();
+            cell.appendChild(document.createTextNode(keys[i]));
+        }
+    }
+
+    // generate table
+    for (i = 0; i < children.length; i++) {
+        let child = children[i];
+        // generate header row
+        if (i === 0) {
+            addHeaders(table, Object.keys(child));
+        }
+        // generate data rows
+        let row = table.insertRow();
+        Object.keys(child).forEach(function (k) {
+            let cell = row.insertCell();
+            cell.appendChild(document.createTextNode(child[k]));
+        })
+    }
+
+    // publish table
+    document.getElementById("container").appendChild(table);
+
+    // developer info
+    console.log("HTML table type: " + typeof table);
+
+
+
+}
+
+// call function
+
+getData();
